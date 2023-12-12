07c0baef5d4864e7a1c3d263928d36f5f7e6d670
Zaher Madi <zahermadi@yahoo.fr>
2023-12-12 17:08:28 +0100

    Réplique des composant html natif en Material UI

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -3,17 +3,41 @@ import { Product } from '../models/product.model';
 import { NgIf,NgFor } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { FormControl, FormGroupDirective, NgForm, Validators,FormsModule, ReactiveFormsModule,} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [NgIf,NgFor, MatCardModule, MatButtonModule],
+  imports: [NgIf,NgFor, MatCardModule, MatButtonModule,MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule],
   templateUrl: "./product-card.component.html",
   styleUrl: "./product-card.component.css"
 })
 
 export class ProductCardComponent{
 @Input() myProduct!: Product;
+selected = new FormControl('valid', [Validators.required, Validators.pattern('valid')]);
+
+selectFormControl = new FormControl('valid', [Validators.required, Validators.pattern('valid')]);
+
+nativeSelectFormControl = new FormControl('valid', [
+  Validators.required,
+  Validators.pattern('valid'),
+]);
+
+matcher = new MyErrorStateMatcher();
 
 
 

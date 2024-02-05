a64e40959cc48e134f8c032ed956af43bd885e78
Zaher Madi <zahermadi@yahoo.fr>
2024-02-05 16:19:52 +0100

    Checkout added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AppComponent } from "./app.component";
 import { CartComponent } from "./cart/cart.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { RedirectionPageComponent } from "./redirection-page/redirection-page.component";
+import { CheckoutComponent } from "./checkout/checkout.component";
 
 export const routes: Routes = [
   
@@ -14,6 +15,6 @@ export const routes: Routes = [
     {path: "Cart" , component: CartComponent},
     {path: "Details/:id" , component: ProductDetailComponent},
     {path: "**" , component: RedirectionPageComponent}
-
+    {path: "checkout", component: CheckoutComponent}
 
 ];
